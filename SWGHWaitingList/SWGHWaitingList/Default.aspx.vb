a19102170ff4b3aab7d957a94aa8e58d3c239cc7
Brennan Thies <11824771+luckycoconut-dev@users.noreply.github.com>
2021-02-16 21:21:03 -0800

    Fit and Finish
    Fixed leading spaces in cells. Added logic to create folder path for answer file in case it is not present or is accidentally deleted by admin.

--- SWGHWaitingList/SWGHWaitingList/Default.aspx.vb
+++ SWGHWaitingList/SWGHWaitingList/Default.aspx.vb
@@ -6,9 +6,10 @@
     End Sub
 
     Protected Sub Button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles Button1.Click
-        Dim file_name As String = "C:\swtools\test.txt"
+        My.Computer.FileSystem.CreateDirectory("C:\swtools")
+        Dim file_name As String = "C:\swtools\formvalues.csv"
         Dim objwriter As New System.IO.StreamWriter(file_name, True)
-        objwriter.Write(TextBox1.Text + ", " + TextBox2.Text + ", " + TextBox3.Text + ", " + TextBox4.Text + vbCrLf)
+        objwriter.Write(TextBox1.Text + "," + TextBox2.Text + "," + TextBox3.Text + "," + TextBox4.Text + vbCrLf)
         objwriter.Close()
         Response.Redirect("~/ThankYou.aspx", True)
     End Sub
